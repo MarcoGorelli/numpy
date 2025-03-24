d278779e72908929fe33c9bd2f5bfeed2e06cdc9
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-03-24 14:07:33 +0000

    TYP: type __int__, __float__, and count for masked arrays

--- numpy/typing/tests/data/pass/ma.py
+++ numpy/typing/tests/data/pass/ma.py
@@ -2,7 +2,10 @@ from typing import Any
 
 import numpy as np
 import numpy.ma
+from typing_extensions import assert_type
 
 
 m: np.ma.MaskedArray[Any, np.dtype[np.float64]] = np.ma.masked_array([1.5, 2, 3], mask=[True, False, True])
 
+assert_type(int(m[1:2]), int)
+assert_type(float(m[1:2]), int)
